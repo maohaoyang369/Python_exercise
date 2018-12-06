9c6af218c14dc5512c965b0c208c0a7097f24988
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2018-12-06 17:40:22 +0800

    Initial commit

--- 钞票换硬币.py
+++ 钞票换硬币.py
@@ -2,3 +2,15 @@
 # -*- coding: utf-8 -*-
 
 
+residue = 100
+count = 0
+result = []
+
+for i in range(int(residue/1)):
+    for m in range(int(residue/2)):
+        for n in range(int(residue/5)):
+            if (i + m * 2 + n * 5) == 100 and i != 0 and m != 0 and n != 0:
+                count += 1
+                result.append((i, m, n))
+print(count)
+print(result)

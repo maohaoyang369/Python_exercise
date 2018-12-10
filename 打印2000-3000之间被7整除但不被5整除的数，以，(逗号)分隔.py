f3982fabec1b2aee1a5524236eb86fc9d921534a
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2018-12-10 16:04:19 +0800

    Initial commit

--- 打印2000-3000之间被7整除但不被5整除的数，以，(逗号)分隔.py
+++ 打印2000-3000之间被7整除但不被5整除的数，以，(逗号)分隔.py
@@ -4,9 +4,11 @@
 # 打印2000-3000之间被7整除但不被5整除的数，以，(逗号)分隔
 
 def num_senven():
-    for i in range(2000,3001):
+    result = []
+    for i in range(2000, 3001):
         if i % 7 == 0 and i % 5 != 0:
-            print(i)
-    return i
+            # print(i)
+            result.append(str(i))
+    return ",".join(result)
 
 print(num_senven())

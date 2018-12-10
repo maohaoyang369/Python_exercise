2e360bf8eabe439c0ad362a9ad3a915824cbe522
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2018-12-10 11:33:56 +0800

    Initial commit

--- 自己实现在一句话中查找某个单词的算法.py
+++ 自己实现在一句话中查找某个单词的算法.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-sentance = "Please find the words."
+sentance = "Please find the words"
 
 
 def find_word(sentance, word):
-    sentance_len = len(sentance)
+    sentance_len = len(word)
     print(sentance_len)
-    sentance_list = sentance.split(" ")
-    print(sentance_list)
-    for i in range(sentance_list):
-        print(i)
-        if i == word:
-            return "\"\""
-        else:
-            return False
+    for i in range(len(sentance)):
+        if sentance[i:i+sentance_len] == word:
+            return sentance[i:i+sentance_len]
+    return False
 
 print(find_word(sentance, 'the'))
+print(find_word(sentance, 'dound'))

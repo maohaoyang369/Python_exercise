37e3436254140f9decf1eab4630e5377bc887c70
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2018-12-12 17:29:53 +0800

    Initial commit

--- 编程将类似“China”这样的明文译成密文.py
+++ 编程将类似“China”这样的明文译成密文.py
@@ -5,4 +5,19 @@
 # 密码规律是：用字母表中原来的字母后面第4个字母代替原来的字母，不能改变其大小写，
 # 如果超出了字母表最后一个字母则回退到字母表中第一个字母
 
+import string
 
+
+def encrypt(sentance):
+    sentance_encrypt = ""
+    for i in sentance:
+        # print(i)
+        if i in string.ascii_letters and (ord(i)+4 >= ord('a') and ord(i)+4 <= ord('z')) or (ord(i)+4 >= ord('A') and ord(i)+4 <= ord('Z')):
+            i = i.replace(i, chr(ord(i) + 4))
+            # print(i)
+        else:
+            i = i.replace(i, chr(ord(i) - 22))
+        sentance_encrypt += i
+    return sentance_encrypt
+
+print(encrypt("China"))

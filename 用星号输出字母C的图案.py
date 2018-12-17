1e3fe6d366db2c9204ef8754f5ba3e7e32c74ca4
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2018-12-17 17:18:55 +0800

    Initial commit

---
+++ 用星号输出字母C的图案.py
@@ -0,0 +1,10 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-!
+
+# 用*号输出字母c的图案
+
+for i in range(1, 10):
+    if i == 4 or i == 5 or i == 6:
+        print("* "*3)
+    else:
+        print("* "*10)
---
+++ 画倒等边三角形.py
@@ -0,0 +1,5 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-!
+
+for i in range(5, 0, -1):
+    print("* " * i)

2ce41f94e0ddea763db5cfe7833603c64a62dae6
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2018-12-17 16:07:27 +0800

    Initial commit

---
+++ 画等（腰）边三角形（实心、空心）.py
@@ -0,0 +1,29 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-!
+
+# 等边三角形（实心）
+
+for i in range( 5):
+    print("* " * i)
+
+# 等边三角形（空心）
+
+print("* ")
+for i in range(1, 4):
+    print("*" + "  "*i + "*")
+    if i == 3:
+        print("*  "*4)
+
+# 等腰三角形（实心）
+
+for i in range(6):
+    print("*" * i)
+
+# 等腰三角形（空心）
+
+print("*")
+for i in range(6):
+    print("*" + "  "*i + "*")
+    if i == 5:
+        print("* " * 7)
+

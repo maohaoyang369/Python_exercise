d27b03cc3f5f58e69ed7a3e7a099f6e0a5d46fdd
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2018-12-29 10:47:41 +0800

    Initial commit

--- 将一个多重嵌套的列表的元素进行互换，存到另一个同等维度的嵌套列表中.py
+++ 将一个多重嵌套的列表的元素进行互换，存到另一个同等维度的嵌套列表中.py
@@ -5,7 +5,7 @@
 # 例如：[[1,2,3],[4,5,6]]互换后变成[[1,4],[2,5],[3,6]]
 
 sentance = [[1, 2, 3], [4, 5, 6]]
-result = [[0,0], [0,0], [0,0]]
+result = [[0, 0], [0, 0], [0, 0]]
 
 for i in range(len(sentance)):
     # print(i)
@@ -13,10 +13,7 @@ for i in range(len(sentance)):
         # print(j)
         print(sentance[i][j])
         result[j][i] = sentance[i][j]
-    # result[2].append(sentance[i][j])
 print(result)
-    # print(i[0],i[1])
-    # for m in i:
-    #     print(m)
+
 
 
--- 有一个3 x 4的矩阵，要求编程求出其中值最大的那个元素的值，以及其所在的行号和 列号，矩阵可以通过嵌套列表来模拟.py
+++ 有一个3 x 4的矩阵，要求编程求出其中值最大的那个元素的值，以及其所在的行号和 列号，矩阵可以通过嵌套列表来模拟.py
@@ -4,3 +4,18 @@
 # 有一个3 x 4的矩阵，要求编程求出其中值最大的那个元素的值，
 # 以及其所在的行号和列号，矩阵可以通过嵌套列表来模拟
 
+
+# 遍历嵌套列表的每一个元素
+# 与初始变量值进行比较
+# 保存下最大的元素
+
+array = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+max_value = array[0][0]
+
+for i in range(len(array)):
+    for j in range(array[i]):
+        print(j)
+        if array[i][j] > max_value:
+            max_value = array[i][j]
+print(max_value)
+

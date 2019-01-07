890c0851a74ba0d69d907c340a487f0cd7688cb6
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2019-01-07 17:37:47 +0800

    Initial commit

--- 一个包含多个数字的列表，请使用随机的方式，将每个数字+1后，生成新列表.py
+++ 一个包含多个数字的列表，请使用随机的方式，将每个数字+1后，生成新列表.py
@@ -3,5 +3,16 @@
 
 # 一个包含多个数字的列表，请使用随机的方式，将每个数字+1后，生成新列表
 
+import random
+
 sentence = [1, 2, 3, 4, 55, 6, 6, 77]
+random.shuffle(sentence)
+result = []
+
+for i in sentence:
+    result.append(i+1)
+
+print(result)
+
+
 

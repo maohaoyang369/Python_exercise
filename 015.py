ffb66033a025e4e5eac3a1991ee697becc414440
maohaoyang369 <37652726+maohaoyang369@users.noreply.github.com>
2019-01-29 15:58:41 +0800

    Update commit

--- 015.py
+++ 015.py
@@ -3,8 +3,8 @@
 
 # 分离list1与list2中相同部分与不同部分
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [3, 4, 5, 6, 7]
+list1 = [1, 2, 3, 4, 5, 100]
+list2 = [3, 4, 5, 6, 7, 8, 9, 100]
 
 same = []
 different = []
@@ -13,7 +13,15 @@ for i in list1:
     for j in list2:
         if i == j:
             same.append(i)
-        elif i != j and:
-            different.append(i)
+    if i not in same:
+        different.append(i)
+
+for m in list2:
+    for n in list1:
+        if m == n and m not in same:
+            same.append(m)
+    if m not in same:
+        different.append(m)
+
 print(same)
 print(different)

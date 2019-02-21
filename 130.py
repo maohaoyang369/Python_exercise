56b6ab496bcd8940876f960cdaf57b9c8bbcd729
maohaoyang369 <372713573@qq.com>
2019-02-21 23:26:07 +0800

    Update commit

--- 130.py
+++ 130.py
@@ -3,13 +3,15 @@
 
 # 统计名字列表中，各名字的首字母在名字列表中出现的次数
 
-name = ["Alice", "Monica", "Joha", "Ocean", "Peter", "Mike"]
-name_count = {}
-result = 0
+name = ["Alice", "Monica", "Joha", "Ocean", "Peter", "Mike", "Jon", "Msfsd"]
+name_count = []
+result = {}
 for i in name:
-    # print(i)
-    # print(i[0])
-    name_count[i[0]] = 0
-if name_count[i[0]] in name_count.keys():
-    name_count[i[0]] += 1
-print(name_count)
+    name_count.append(i[0])
+for j in name_count:
+    # print(j)
+    result.setdefault(j, 0)     # setdefault 键不存在于字典中，将会添加键和指定的值
+    result[j] += 1
+
+# print(name_count)
+print(result)

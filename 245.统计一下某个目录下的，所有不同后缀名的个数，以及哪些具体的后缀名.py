def9b807460a2c70b5e72639ceaee5985c805085
Monica <372713573@qq.com>
2019-04-11 17:24:58 +0800

    Update commit

--- 245.统计一下某个目录下的，所有不同后缀名的个数，以及哪些具体的后缀名.py
+++ 245.统计一下某个目录下的，所有不同后缀名的个数，以及哪些具体的后缀名.py
@@ -1,4 +1,20 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# 
+# 统计一下某个目录下的，所有不同后缀名的个数，以及哪些具体的后缀名
+
+import os
+import os.path
+
+
+def get_postfix_name_count(dir_path):
+    result = []
+    for root, dirs, files in os.walk(dir_path):
+        for i in files:
+            postfix_name = os.path.splitext(os.path.join(root, i))[1]
+            if postfix_name != "":
+                result.append(postfix_name[1:])
+    return list(set(result)), len(set(result))
+
+
+print(get_postfix_name_count("d:\\python"))
